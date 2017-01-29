260a0ed81c72903a9ce07fb1dc03b6fa1181c4fc
Steven Malis <smmalis37@gmail.com>
2017-01-28 18:18:40 -0800

    Python 3.5 support and elevator support

--- bot.py
+++ bot.py
@@ -2,26 +2,23 @@
 import discord
 import asyncio
 
-
 friendName = "Old Friend"
-
+elevatorName = "Elevator"
+elevator = None
 
 client = discord.Client()
-
 
 discord_colors = discord.Color.__dict__
 colors = list(filter(lambda x: isinstance(discord_colors[x],classmethod), discord_colors))
 colors.sort()
 
-
-@asyncio.coroutine
-def handle_color(message):    
+async def handle_color(message):    
     words = message.content.split(' ')
     if len(words) == 1:
-        yield from client.send_message(message.channel, 'Available colors are:\n' + (', '.join(colors)))
+        await client.send_message(message.channel, 'Available colors are:\n' + (', '.join(colors)))
     if len(words) == 2:
         if (words[1] not in colors):
-            yield from client.send_message(message.channel, 'That is not a valid color.')
+            await client.send_message(message.channel, 'That is not a valid color.')
         else:
             author = message.author
 
@@ -31,16 +28,15 @@ def handle_color(message):
             color_role = list(filter(lambda r: r.name == words[1], message.server.roles))[0]
             new_roles = old_roles + [color_role]
             
-            yield from client.replace_roles(author, *new_roles)
-            yield from client.send_message(message.channel, "Changed your color to " + words[1])
+            await client.replace_roles(author, *new_roles)
+            await client.send_message(message.channel, "Changed your color to " + words[1])
 
 @client.async_event
-def on_server_role_update(before, after):
+async def on_server_role_update(before, after):
     if (after.is_everyone):
-        yield from refresh_roles(after.server)
+        await refresh_roles(after.server)
             
-@asyncio.coroutine
-def refresh_roles(server):
+async def refresh_roles(server):
     print("Refreshing roles for " + server.name)
     
     perms = discord.Permissions(server.default_role.permissions.value)
@@ -48,23 +44,22 @@ def refresh_roles(server):
     to_edit = list(filter(lambda x: x.permissions != perms, filter(lambda x: x.name in colors or x.name == friendName, server.roles)))
     
     for edit in to_edit:
-        yield from client.edit_role(server, edit, permissions=perms)
+        await client.edit_role(server, edit, permissions=perms)
         print("Edited " + edit.name)
 
     perms.read_message_history = False
     if (server.default_role.permissions != perms):
-        yield from client.edit_role(server, server.default_role, permissions=perms)
+        await client.edit_role(server, server.default_role, permissions=perms)
         print("Edited @everyone")
         
-@asyncio.coroutine
-def create_roles(server):
+async def create_roles(server):
     print("Creating roles for " + server.name)
     
     role_names = list(map(lambda r: r.name, server.roles))
     to_create = list(filter(lambda x: x not in role_names, colors))
     
     for color in to_create:
-        yield from client.create_role(server, name=color, color=getattr(discord.Color, color)())
+        await client.create_role(server, name=color, color=getattr(discord.Color, color)())
         print("Created " + color)
         
 def is_admin(member):
@@ -74,37 +69,46 @@ def is_friend(member):
     return friendName in map(lambda r: r.name, member.roles)
 
 @client.async_event
-def on_message(message):
+async def on_message(message):
     if (is_admin(message.author) or is_friend(message.author)):
         words = message.content.split(' ')
         command = words[0]
         
         if command == '!help':
-            yield from client.send_message(message.channel, "Available commands:\n!help, !color")
+            await client.send_message(message.channel, "Available commands:\n!help, !color")
             if is_admin(message.author):
-                yield from client.send_message(message.channel, "Admin-only commands:\n")
+                await client.send_message(message.channel, "Admin-only commands:\n")
                 
         if command == '!color':
-            yield from handle_color(message)
+            await handle_color(message)
                     
         if command =='!debug':
             if message.author.id == '123301224022933504':
                 try:
-                    command = ' '.join(words[1:])
-                    print("Evaling " + command)
-                    result = eval(command)
+                    to_eval = ' '.join(words[1:])
+                    print("Evaling `" + to_eval + "`")
+                    result = eval(to_eval)
                 except Exception as e:
                     result = '{0.__name__}: {1}'.format(type(e), e)
-                    yield from client.send_message(message.channel, "'''" + result + "'''")
-            
+                await client.send_message(message.channel, "```" + str(result) + "```")
+
+async def create_elevator(client, server):
+    global elevator
+    voice_channel = discord.utils.get(server.channels, name=elevatorName)
+    voice_client = await client.join_voice_channel(voice_channel)
+    elevator = await voice_client.create_ytdl_player("https://www.youtube.com/watch?v=VBlFHuCzPgY")
+    elevator.volume = .3
+    elevator.start()
+                        
 @client.async_event
-def on_ready():
+async def on_ready():
     print('Logged in!')
 
-    for server in client.servers:
-        yield from create_roles(server)
-        yield from refresh_roles(server)
-        
+    server = list(client.servers)[0]
+    await create_roles(server)
+    await refresh_roles(server)
+    await create_elevator(client, server)
+    
     print('Ready')
 
 
