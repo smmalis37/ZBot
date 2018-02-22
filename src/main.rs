3130e80015a4d6291d6febeb81173ecdb8ca13d7
Steven Malis <smmalis37@gmail.com>
2018-02-21 18:25:03 -0800

    Switch to a Command trait.

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,17 @@ use rand::Rng;
 use serenity::prelude::*;
 use serenity::model::prelude::*;
 
-type CommandMap = BTreeMap<&'static str, Command>;
-type Command = fn(ctx: &Context, msg: &Message, args: &[&str]);
+type CommandMap = BTreeMap<&'static str, Box<Command + Sync>>;
+trait Command {
+    fn exec(&self, ctx: &Context, msg: &Message, args: &[&str]);
+}
 
 lazy_static! {
     static ref COMMAND_MAP: CommandMap = {
         let mut commands = CommandMap::new();
-        commands.insert("help", help);
-        commands.insert("flip", flip);
-        commands.insert("roll", roll);
+        commands.insert("help", Box::new(Help));
+        commands.insert("flip", Box::new(Flip));
+        commands.insert("roll", Box::new(Roll));
         commands
     };
 }
@@ -46,7 +48,7 @@ impl EventHandler for Handler {
 
             if let Some(command) = maybe_command {
                 if let Some(command_handler) = COMMAND_MAP.get(command) {
-                    command_handler(&ctx, &msg, &*args);
+                    command_handler.exec(&ctx, &msg, &*args);
                 } else {
                     msg.reply(&format!("Unknown command: {}", command)).unwrap();
                 }
@@ -57,58 +59,67 @@ impl EventHandler for Handler {
     }
 }
 
-fn help(_ctx: &Context, msg: &Message, args: &[&str]) {
-    if args.len() != 0 {
-        msg.reply("This command does not accept any arguments.")
-            .unwrap();
-        return;
-    }
-
-    let help_output = COMMAND_MAP
-        .keys()
-        .intersperse(&", !")
-        .fold("Available commands:\n!".to_owned(), |output, command| {
-            output + command
-        });
-
-    msg.channel_id.say(&help_output).unwrap();
-}
-
-fn flip(_ctx: &Context, msg: &Message, args: &[&str]) {
-    if args.len() != 0 {
-        msg.reply("This command does not accept any arguments.")
-            .unwrap();
-        return;
-    }
-
-    if rand::thread_rng().gen() {
-        msg.reply("Heads!").unwrap();
-    } else {
-        msg.reply("Tails!").unwrap();
-    }
-}
-
-fn roll(_ctx: &Context, msg: &Message, args: &[&str]) {
-    let upper_bound = if args.len() == 0 {
-        6
-    } else if args.len() == 1 {
-        if let Ok(num) = args[0].parse() {
-            num
-        } else {
-            msg.reply("That is not a number.").unwrap();
+struct Help;
+impl Command for Help {
+    fn exec(&self, _ctx: &Context, msg: &Message, args: &[&str]) {
+        if args.len() != 0 {
+            msg.reply("This command does not accept any arguments.")
+                .unwrap();
             return;
         }
-    } else {
-        msg.reply("This command only accepts one argument.")
-            .unwrap();
-        return;
-    };
 
-    if upper_bound <= 0 {
-        msg.reply("The upper bound must be a positive number.")
-            .unwrap();
-    } else {
-        let result = rand::thread_rng().gen_range(0, upper_bound) + 1;
-        msg.reply(&format!("{}", result)).unwrap();
+        let help_output = COMMAND_MAP
+            .keys()
+            .intersperse(&", !")
+            .fold("Available commands:\n!".to_owned(), |output, command| {
+                output + command
+            });
+
+        msg.channel_id.say(&help_output).unwrap();
+    }
+}
+
+struct Flip;
+impl Command for Flip {
+    fn exec(&self, _ctx: &Context, msg: &Message, args: &[&str]) {
+        if args.len() != 0 {
+            msg.reply("This command does not accept any arguments.")
+                .unwrap();
+            return;
+        }
+
+        if rand::thread_rng().gen() {
+            msg.reply("Heads!").unwrap();
+        } else {
+            msg.reply("Tails!").unwrap();
+        }
+    }
+}
+
+struct Roll;
+impl Command for Roll {
+    fn exec(&self, _ctx: &Context, msg: &Message, args: &[&str]) {
+        let upper_bound = if args.len() == 0 {
+            6
+        } else if args.len() == 1 {
+            if let Ok(num) = args[0].parse() {
+                num
+            } else {
+                msg.reply("That is not a number.").unwrap();
+                return;
+            }
+        } else {
+            msg.reply("This command only accepts one argument.")
+                .unwrap();
+            return;
+        };
+
+        if upper_bound <= 0 {
+            msg.reply("The upper bound must be a positive number.")
+                .unwrap();
+        } else {
+            let result = rand::thread_rng().gen_range(0, upper_bound) + 1;
+            msg.reply(&format!("{}", result)).unwrap();
+        }
     }
 }
